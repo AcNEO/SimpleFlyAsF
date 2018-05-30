2c3466227f832ef6f464044afa3574634df29e60
PrimordialCake <37327658+AcNEO@users.noreply.github.com>
2018-05-30 17:59:15 +0800

    idek what i do

--- src/AcNEO/SimpleFlyAsF/Main.php
+++ src/AcNEO/SimpleFlyAsF/Main.php
@@ -2,9 +2,31 @@
 
 namespace AcNEO\SimpleFlyAsF;
 
-class Main {
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
 
-
+class Main extends PluginBase implements Listener {
+  
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+  $cmd = strtolower($command->getName());
+    switch($cmd) {
+      case "fly":
+        if($sender->hasPermission("simple.fly.oof") {
+         $this->player->setAllowFlight(true);
+        }
+        break;
+       case "nofly":
+        if($sender->hasPermission("simple.fly.oof") {
+         $this->player->setAllowFlight(false);
+        }
+        break;
+    }
+  }
 
 }
 
